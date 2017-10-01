cd00b2ff689e06bac2553c656ab9ac2ba076c69a
Giampiero Specogna <giampi_12@hotmail.com>
2017-10-01 11:58:59 -0400

    Some validations

--- public/js/Empleado/insertEmpleado.js
+++ public/js/Empleado/insertEmpleado.js
@@ -5,6 +5,25 @@
             el:"#app",
             data:{
                 dependencies: dep
+            },
+            methods:{
+                limitCedula: function (e) {
+                    var span = document.getElementById('cedulaLimit');
+                    var cedula = document.getElementById('cedula');
+                    var len = cedula.value.toString().length;
+                    if (len >= 11) {
+                        e.preventDefault();
+                        span.innerHTML = "Ha alcanzado la cantidad de 11 digitos permitidos";
+                    }
+                },
+                eraseCedulaSpan: function () {
+                    var span = document.getElementById('cedulaLimit');
+                    var cedula = document.getElementById('cedula');
+                    var len = cedula.value.toString().length;
+                    if (len >= 2 && len < 11) {
+                        span.innerHTML = "";
+                    }
+                }
             }
         });
     });
--- public/js/inspeccion/insertInsp.js
+++ public/js/inspeccion/insertInsp.js
@@ -5,6 +5,26 @@
             el: '#app',
             data: {
                 dependencies: dep
+            },
+            methods:{
+                limitGalons: function(e){
+                    var combustible = document.getElementById('combustible');
+                    var span = document.getElementById('combustibleLimit');
+                    var maxAmount = parseInt(combustible.value);
+                    if(maxAmount >= 100){
+                        e.preventDefault();
+                        span.innerHTML = "Ha excedido la cantidad maxima de 100 galones";
+                        combustible.value = "";
+                    }
+                },
+                eraseSpan: function(){
+                    var combustible = document.getElementById('combustible');
+                    var span = document.getElementById('combustibleLimit');
+                    var maxAmount = parseInt(combustible.value);
+                    if (maxAmount < 100) {
+                        span.innerHTML = "";
+                    }
+                }
             }
         });
     });

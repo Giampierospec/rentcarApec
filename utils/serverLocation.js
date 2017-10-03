cafd3837cacd5f2743c734f1e638e3d3c68be6da
Giampiero Specogna <giampi_12@hotmail.com>
2017-10-02 22:04:40 -0400

    Changed Url

--- utils/serverLocation.js
+++ utils/serverLocation.js
@@ -2,7 +2,7 @@ var apiOptions = {
     server: 'http://127.0.0.1:3000'
 };
 if (process.env.NODE_ENV === 'production') {
-    apiOptions.server = 'https://rentcarapec.herokuapp.com';
+    apiOptions.server = 'http://carapec.azurewebsites.net';
 }
 
 module.exports = apiOptions;

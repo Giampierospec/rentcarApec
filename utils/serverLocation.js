881ae65342c50eec5c4b4fdbfca2bb6e2183baf9
Giampiero Specogna <giampi_12@hotmail.com>
2017-10-07 19:07:00 -0400

    Added a dynamic message

--- utils/serverLocation.js
+++ utils/serverLocation.js
@@ -2,7 +2,7 @@ var apiOptions = {
     server: 'http://127.0.0.1:3000'
 };
 if (process.env.NODE_ENV === 'production') {
-    apiOptions.server = 'https://rentcarapec.herokuapp.com';
+    apiOptions.server = process.env.SERVER_URI;
 }
 
 module.exports = apiOptions;

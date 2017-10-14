4b8e77554b1aea049f61375ff7837aaf88ca728a
Giampiero Specogna <giampi_12@hotmail.com>
2017-10-14 08:29:28 -0400

    Added link click distribution

--- public/js/vehiculos/sendToInspeccion.js
+++ public/js/vehiculos/sendToInspeccion.js
@@ -1,11 +1,11 @@
 (function(){
-  $('#insp').click(function(e, btn){
+  $(document).on('click','.insp',function(e){
       e.preventDefault();
       var url = this.href.split('/');
       var vehiculoId = url[3];
       $.post('/api/setEstadoVehiculo',{vehiculoId: vehiculoId})
         .done(function(data){
-            alert('El estado de vehiculo ha pasado ha ser de Inspeccion');
+            alert('El estado de vehiculo ha pasado a ser de Inspeccion');
             location.reload();
         });
   });
